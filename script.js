6a9f772d7d6b971a8e739049d44dc6ed1c818140
Fredrik Andersson <fredrik.andersson1@chasacademy.se>
2024-12-18 13:57:39 +0100

    fixat sökfunctionen och lite css

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const apikey = "b7dd0caa";
-let search = "Lord of the rings";
+let search = "Batman";
 let year = 2010;
 let movies = []; // array of IMDB ids. To use in the function moviesWithFullInfo()
+let moviesWithDetail = [];
 
 // function search movies and takes out the imdb ID.
 async function movieDataSearch() {
@@ -21,85 +22,105 @@ async function movieDataSearch() {
       imdbId: item.imdbID,
     }));
     movies.push(...mappedData);
-
-    // listMovies(movies);
+    moviesWithFullInfo();
   } catch (error) {
     console.error(error);
   }
-  moviesWithFullInfo();
 }
 
-console.log(movies);
+// console.log(movies);
 
 async function moviesWithFullInfo() {
-  movies.forEach((movie) => {
-    const id = movie.imdbID;
-    console.log("dadawdawd", id); // Skriv ut varje imdbId
-  });
-  try {
-    let response = await fetch(
-      `http://www.omdbapi.com/?i=${movies.imdbID}&type=movie&plot=full&apikey=${apikey}`
-    );
-    if (!response.ok) {
-      throw new Error(`Error ${response.statusText}`);
+  for (const movie of movies) {
+    try {
+      const id = movie.imdbId; // Correctly access the imdbId
+      console.log("Fetching data for IMDb ID:", id); // Debugging log
+
+      let response = await fetch(
+        `http://www.omdbapi.com/?i=${id}&type=movie&plot=full&apikey=${apikey}`
+      );
+      if (!response.ok) {
+        throw new Error(`Error ${response.statusText}`);
+      }
+      let data = await response.json();
+
+      let detailData = {
+        genre: data.Genre, // Extract genre
+        title: data.Title, // Extract title
+        actors: data.Actors,
+        img: data.Poster,
+        runtime: data.Runtime,
+        year: data.Year,
+        writer: data.Writer,
+        imdb: data.imdbID,
+      };
+
+      moviesWithDetail.push(detailData);
+      console.log("MOOOOVIES", moviesWithDetail);
+    } catch (error) {
+      console.error("Error fetching movie info:", error);
     }
-    let data = await response.json();
-    console.log(`Fullständiga Filmer ${data}`);
-  } catch (error) {}
+  }
+  listMovies(moviesWithDetail);
 }
+
+// Call the search function
 movieDataSearch();
-// async function movieData() {
-//   try {
-//     let response = await fetch(
-//       `http://www.omdbapi.com/?s=${search}&y=${year}&apikey=${apikey}`
-//     );
 
-//     if (!response.ok) {
-//       throw new Error(
-//         `Error i hämtning av API: ${response.status} ${response.statusText}`
-//       );
-//     }
+function listMovies(moviesWithDetail) {
+  const uL = document.getElementsByClassName("movieList")[0];
+  for (let i = 0; i < moviesWithDetail.length; i++) {
+    const detailData = moviesWithDetail[i];
+    const movieListItem = document.createElement("li");
+    movieListItem.classList.add("listCard");
+    movieListItem.innerHTML = `
+        <img id="imgCard" src="${detailData.img}" alt="poster för ${detailData.title}">
+        <p>${detailData.title}</p>
+        <p>${detailData.actors}</p>
+        <p>${detailData.runtime}</p>
+        <p>${detailData.year}</p>
+        <p>${detailData.writer}</p>
+        <a href="https://www.imdb.com/title/${detailData.imdb}/" target="_blank"><button>Läs mer på imdb</button></a>
+        `;
+    console.log(detailData.title);
 
-//     let data = await response.json();
-//     let mappedData = data.Search.map((item) => ({
-//       poster: item.Poster,
-//       title: item.Title,
-//       year: item.Year,
-//       imdbId: item.imdbID,
-//     }));
-//     movies.push(...mappedData);
-//     // listMovies(movies);
-//     // console.log(data);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+    uL.appendChild(movieListItem);
+  }
+}
 
-// // movieData();
-
-// function listMovies(movies) {
+const searchForm = document.getElementById("searchForm");
+const searchInput = document.getElementById("search");
+async function searchNewMovies() {
+  searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = searchInput.value;
+    search = input;
+    movies = []; // tömmer arrayen innan sökning.
+    moviesWithDetail = []; // tömmer array
+    let uL = document.getElementsByClassName("movieList")[0]; // klass för listan
+    uL.innerHTML = ""; // Töm listan innan vi lägger till nya resultat
+    movieDataSearch();
+    console.log(input);
+    searchInput.value = "";
+  });
+}
+searchNewMovies();
+// function listMovies(moviesWithDetail) {
 //   const uL = document.getElementsByClassName("movieList")[0];
-//   movies.forEach((movieList) => {
+//   moviesWithDetail.forEach((detailData) => {
 //     const movieListItem = document.createElement("li");
 //     movieListItem.classList.add("listCard");
 //     movieListItem.innerHTML = `
-//         <img id="imgCard" src="${movieList.poster}" alt="poster för ${movieList.title}">
-//         <p>${movieList.title}</p>
-//         <p>${movieList.year}</p>
-//         <a href="https://www.imdb.com/title/${movieList.imdbId}/"target="_blank"><button>Mer info</button></a>
+//         <img id="imgCard" src="${detailData.Poster}" alt="poster för ${detailData.Title}">
+//         <p>${detailData.title}</p>
+//         <p>${detailData.Actors}</p>
+//         <p>${detailData.Runtime}</p>
+//         <p>${detailData.Year}</p>
+//         <p>${detailData.writer}</p>
+//         <a href="https://www.imdb.com/title/${detailData.imdb}/"target="_blank"><button>Läs mer på imdb</button></a>
 //         `;
+//     console.log(detailData.title);
+
 //     uL.appendChild(movieListItem);
 //   });
 // }
-
-// const searchForm = document.getElementById("searchForm");
-// const searchInput = document.getElementById("search");
-
-// searchForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const input = searchInput.value;
-//   search = input;
-//   // movieDataSearch();
-//   uL.innerHTML = ""; // Töm listan innan vi lägger till nya resultat
-//   console.log(input);
-// });
